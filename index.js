64c6a09616940ae0e60c0ed358a11315efbc5899
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2017-06-16 09:32:28 -0700

    First commit for landing page

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+
+var port = 8080;
+
+app.use(express.static(__dirname));
+
+
+app.listen(port, function(){
+	console.log(`listening on port ${port}`);
+})
